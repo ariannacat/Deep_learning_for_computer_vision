00a250d3d0fb6620b5dca3a5d0d8a042d2a0f338
Arianna Castellini <145551013+ariannacat@users.noreply.github.com>
2025-11-29 00:21:04 +0100

    Update train_constants.py

--- training/train_constants.py
+++ training/train_constants.py
@@ -2,12 +2,12 @@ import os
 
 SEED = int(os.getenv("SEED", "42"))
 BATCH_SIZE = int(os.getenv("BATCH_SIZE","32"))
-NUM_WORKERS = 2
+NUM_WORKERS = int(os.getenv("NUM_WORKERS","2"))
 LR = 3e-4
 WEIGHT_DECAY = 1e-4
-EPOCHS = 30
-VAL_PATIENCE = 5
-WARMUP_EPOCHS = 5
+EPOCHS = int(os.getenv("EPOCHS", "30"))
+VAL_PATIENCE = int(os.getenv("VAL_PATIENCE", "5"))
+WARMUP_EPOCHS = int(os.getenv("WARMUP_EPOCHS", "5"))
 
 # Model
 MODEL_NAME = os.getenv("MODEL_NAME", "resnet18") 
