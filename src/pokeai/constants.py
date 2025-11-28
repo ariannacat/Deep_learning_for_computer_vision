f0cc78648a9d78e94a620c71bee17dc50cb3cef1
Arianna Castellini <145551013+ariannacat@users.noreply.github.com>
2025-11-28 12:27:47 +0100

    Update constants.py

--- src/pokeai/constants.py
+++ src/pokeai/constants.py
@@ -6,11 +6,17 @@ from pathlib import Path
 import os
 import torch
 
+# Directories
 PROJECT_ROOT = Path(os.getenv("POKEAI_PROJECT_ROOT", Path(__file__).resolve().parents[2]))
 CONFIGS_DIR = Path(os.getenv("POKEAI_CONFIGS_DIR", PROJECT_ROOT / "configs"))
 DATASET_DIR = Path(os.getenv("POKEAI_DATASET_DIR", PROJECT_ROOT / "data"))
-DEFAULT_CONFIG_PATH = Path(os.getenv("POKEAI_DEFAULT_CONFIG", CONFIGS_DIR / "default.yaml"))
+ARTIFACTS = Path(os.getenv("POKEAI_ARTIFACTS", PROJECT_ROOT / "artifacts")).resolve()
 
+# Device
 DEVICE = torch.device(os.getenv("POKEAI_DEVICE", "cuda" if torch.cuda.is_available() else "cpu"))
 
-ARTIFACTS = Path(os.getenv("POKEAI_ARTIFACTS", PROJECT_ROOT / "artifacts")).resolve()
+# Default
+DEFAULT_CONFIG_PATH = Path(os.getenv("POKEAI_DEFAULT_CONFIG", CONFIGS_DIR / "default.yaml"))
+DEFAULT_POKEMON_CSV = Path(os.getenv("POKEAI_POKEMON_CSV", DATASET_DIR / "csv/pokemon_data.csv"))
+DEFAULT_MOVESET_CSV = Path(os.getenv("POKEAI_MOVESET_CSV", DATASET_DIR / "csv/movesets.csv"))
+DEFAULT_MOVE_DETAILS_CSV = Path(os.getenv("POKEAI_MOVE_DETAILS_CSV", DATASET_DIR / "csv/moves.csv"))
