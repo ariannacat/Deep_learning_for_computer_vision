dffddbb7e03f6a903f40881da20e998fad54b6ff
Arianna Castellini <145551013+ariannacat@users.noreply.github.com>
2025-12-03 18:58:27 +0100

    Update models.py

--- src/pokeai/models.py
+++ src/pokeai/models.py
@@ -12,7 +12,7 @@ import torch
 import torch.nn as nn
 from torchvision import models
 
-# Common image size + normalization (same as notebook)
+# Common image size + normalization
 IMG_SIZE = 224
 IMAGENET_MEAN = [0.485, 0.456, 0.406]
 IMAGENET_STD = [0.229, 0.224, 0.225]
